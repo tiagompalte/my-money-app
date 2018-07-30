c4bc4e2019a963d1fba50f68e64b2d1f79f8d7e2
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-07-30 19:05:24 -0300

    Create header

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,10 @@
+import '../common/template/dependencies'
+import React from 'react'
+
+import Header from '../common/template/header'
+
+export default props => (
+  <div className='wrapper'>
+    <Header />
+  </div>
+)
