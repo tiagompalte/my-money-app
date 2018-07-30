7ccac4d5402953707d9e14f363dd44e14b165d73
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-07-30 19:25:16 -0300

    Create footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <footer className='main-footer'>
+    <strong>
+      Copyright &copy; 2018
+      <a href='http://github.com/tiagompalte' target='_black'> Tiago M Palte </a>.
+    </strong>
+  </footer>
+)
