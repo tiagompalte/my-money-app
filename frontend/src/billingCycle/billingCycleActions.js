891c21e34ea977da20545bd70ac2b3240ee60c24
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-08-02 15:02:38 -0300

    Implements message like toastr

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
 import Actions from '../common/enum/actions'
 
 const BASE_URL = 'http://localhost:3003/api'
@@ -12,5 +13,14 @@ export function getList() {
 }
 
 export function create(values) {
-  
+  axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+          toastr.success('Sucesso', 'Operação realizada com sucesso')
+        })
+        .catch(e => {
+          e.response.data.errors.forEach(error => toastr.error('Erro', error))          
+        })
+  return {
+    type: 'TEMP'
+  }
 }
