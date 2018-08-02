24c0734f655c191f24625c41e80491d77c45d356
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-08-02 15:14:02 -0300

    dispatch many actions in form

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
+
 import Actions from '../common/enum/actions'
 
 const BASE_URL = 'http://localhost:3003/api'
@@ -13,14 +16,21 @@ export function getList() {
 }
 
 export function create(values) {
-  axios.post(`${BASE_URL}/billingCycles`, values)
+
+  return dispatch => {
+    axios.post(`${BASE_URL}/billingCycles`, values)
         .then(resp => {
           toastr.success('Sucesso', 'Operação realizada com sucesso')
+          dispatch([
+            resetForm('billingCycleForm'),
+            getList(),
+            selectTab('tabList'),
+            showTabs('tabList', 'tabCreate')
+          ])
         })
         .catch(e => {
           e.response.data.errors.forEach(error => toastr.error('Erro', error))          
         })
-  return {
-    type: 'TEMP'
   }
+
 }
