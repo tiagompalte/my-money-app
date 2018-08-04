d9abd69edf87185fe195ac6e815cffde4fb9b542
Tiago Mariani Palte <tiagompalte@hotmail.com>
2018-08-04 17:05:14 -0300

    Show consolidated value in billing cycle list

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import formatCurrency from 'format-currency'
+
 import { getList, showUpdate, showDelete } from './billingCycleActions'
 
+const opts = { format: '%s %v', locale:'pt-BR', symbol: 'R$'}
 class BillingCycleList extends Component {
 
   componentWillMount() {
@@ -10,12 +13,34 @@ class BillingCycleList extends Component {
   }
 
   renderRows() {
-    const list = this.props.list || []
+    const list = this.props.list || []   
+    
+    function calculatedConsolidatedValue(bc) {      
+      var valueCredits = bc.credits.reduce((prev, cur) => prev + cur.value, 0)
+      var valueDebts = bc.debts.reduce((prev, cur) => prev + cur.value, 0)
+      return valueCredits - valueDebts
+    } 
+
+    function changeTextColor(bc) {
+      let value = calculatedConsolidatedValue(bc)
+
+      if(value > 0) {
+        return 'cell-green'
+      } else if(value < 0) {
+        return 'cell-red'
+      } else {
+        return 'cell-blue'
+      }
+    }
+
     return list.map(bc => (
       <tr key={bc._id}>
         <td>{bc.name}</td>
         <td>{bc.month}</td>
         <td>{bc.year}</td>
+        <td className={`${changeTextColor(bc)}`}>
+          {`${formatCurrency(calculatedConsolidatedValue(bc), opts)}`}
+        </td>
         <td>
           <button className='btn btn-warning' onClick={() => this.props.showUpdate(bc)}>
             <i className='fa fa-pencil'></i>
@@ -37,6 +62,7 @@ class BillingCycleList extends Component {
               <th>Nome</th>
               <th>Mês</th>
               <th>Ano</th>
+              <th>Valor Consolidado</th>
               <th className='table-actions'>Ações</th>
             </tr>
           </thead>
